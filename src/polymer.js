d01b1409adf468c2398f0c330a60761caf08b4c0
Addison <addison@framer.com>
2019-08-22 20:42:59 +0200

    Update entry

--- src/polymer.js
+++ src/polymer.js
@@ -1,3 +1,7 @@
 console.log(
   "Welcome to Polymer, a Web Component library of components to be used in Framer X."
 );
+
+console.log(
+  "Feel free to make a Pull Request with any suggestions or feature requests."
+);
